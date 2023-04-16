0f7c1c73c00a79793a7efb03a763ecbf6a6be251
masnzzz <z3heng0326@gmail.com>
2023-04-16 23:55:22 +0900

    feat: Blog関連ルーティング追加 記事一覧, 記事詳細, 記事作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # ヘルスチェック
+  get "/health_check", to: "health_check#index"
+  # 記事一覧
+  get "/blogs", to: "blogs#index"
+  # 記事詳細
+  get "/blogs/:id", to: "blogs#show"
+  # 記事作成
+  post "/blogs", to: "blogs#create"
 end
