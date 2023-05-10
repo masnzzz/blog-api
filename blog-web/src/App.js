3819b59673326da5cb65ff37c4ae69a5238617a3
masnzzz <z3heng0326@gmail.com>
2023-05-10 18:15:22 +0900

    feat: top画面にリンクを設置
    記事作成画面と記事一覧画面のリンクを作成

--- blog-web/src/App.js
+++ blog-web/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "auto", width: "1000px" }}>
+      <h1>ブログアプリ</h1>
+      <div>
+        <Link to="/create">記事作成画面</Link>
+      </div>
+      <div>
+        <Link to="/blogs">記事一覧画面</Link>
+      </div>
     </div>
   );
 }
