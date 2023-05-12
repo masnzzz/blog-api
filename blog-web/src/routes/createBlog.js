34b2ad288c1122c75e89148feaf827eda70351e5
masnzzz <z3heng0326@gmail.com>
2023-05-12 12:27:21 +0900

    feat: 記事作成画面の入力欄を実装

--- blog-web/src/routes/createBlog.js
+++ blog-web/src/routes/createBlog.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const CreateBlog = () => {
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
   return (
     <div style={{ margin: "auto", width: "1000px" }}>
       <h1>ブログ作成画面</h1>
@@ -10,8 +13,24 @@ const CreateBlog = () => {
       <div>
         <Link to="/blogs">記事一覧画面</Link>
       </div>
+      <h2>タイトルを入力してください</h2>
+      <div>
+        <input
+          value={title}
+          onChange={(element) => setTitle(element.target.value)}
+          style={{ width: "500px", height: "300px" }}
+        />
+      </div>
+      <h2>本文を入力してください</h2>
+      <div>
+        <textarea
+          value={contents}
+          onChange={(element) => setContents(element.target.value)}
+          style={{ width: "500px", height: "300px" }}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default CreateBlog;
