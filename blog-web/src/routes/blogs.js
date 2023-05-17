a6a8c20d629af1ec238052cd342fb74788b24753
masnzzz <z3heng0326@gmail.com>
2023-05-17 12:20:04 +0900

    feat: 記事一覧機能の実装

--- blog-web/src/routes/blogs.js
+++ blog-web/src/routes/blogs.js
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../utils/axios";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState();
+  useEffect(() => {
+    const f = async () => {
+      const response = await axiosInstance.get("/blogs");
+      setBlogs(response.data);
+    };
+    f();
+  }, []);
+
   return (
     <div style={{ margin: "auto", width: "1000px" }}>
       <h1>ブログ一覧画面</h1>
@@ -9,6 +20,16 @@ const Blogs = () => {
       </div>
       <div>
         <Link to="/create">記事作成画面</Link>
+      </div>
+
+      <div>
+        <ul>
+          {blogs?.map((blog) => (
+            <Link to={`/blogs/${blog.id}`} key={blog.id}>
+              <li>{blog.title}</li>
+            </Link>
+          ))}
+        </ul>
       </div>
     </div>
   )
